d811264cc2a335974c062b5e9ae4450c6d88b449
Amarnath P <96828744+amarnathp-o2o@users.noreply.github.com>
2022-01-01 13:06:37 +0530

    update main.js

--- libs/main.js
+++ libs/main.js
@@ -1,6 +1,19 @@
 if (ed.getSdk() == "1") {
   if(document.URL.indexOf("educlick.page") <= -1){
-     alert("test done");
+    if (ed.getData() == '') {
+      ed.save('1')
+      alert('this is the first non-educlick link u visiting')
+    } else if (ed.getData() == '1') {
+      ed.save('0')
+      setInterval(function() {
+          alert('this is the second non-educlick link u visiting')
+      }, 2000);
+    }
+  } else {
+    ed.clearData();
+    if (ed.getData() == '0') {
+      ed.save('')
+    }
   }
 } else {
   alert("the adClicker you are using is unsupported, kindly update or contant my author");
